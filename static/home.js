09388b8d0c8e91f94b87bfd1a2e753dd06d9df82
Angelica M <angelica.michacaco@gmail.com>
2022-05-02 08:26:43 -0400

    added styling to buttons using golden rod hover accents

--- static/home.js
+++ static/home.js
@@ -1,6 +1,6 @@
 //This function dynamically creates the three cocktail cards on the home page
 function featureCocktails(){
-  let new_card = $("<div class = 'col-md-4 '><div class='card custom-card customHomeCard center-text'> <div class = 'ratio-images'> <img class='card-img-top' src='" + cocktails[drink]['image'] + "' alt='card image cap' /> </div> <div class='card-body'> <div class='btn-container'> <a href=" + cocktails[drink]['learnUrl'] + "><button class='btn-in-home-cards learn-drink-btn'>" + cocktails[drink]['name']+ "</button></a> <a  href=" + cocktails[drink]['quizUrl'] + "><button class='take-quiz-btn btn-in-home-cards'>Take the Quiz</button></a></div></div></div></div>") 
+  let new_card = $("<div class = 'col-md-4 '><div class='card custom-card customHomeCard center-text'> <div class = 'ratio-images'> <img class='card-img-top' src='" + cocktails[drink]['image'] + "' alt='card image cap' /> </div> <div class='card-body'> <div class='btn-container'> <a href=" + cocktails[drink]['learnUrl'] + "><button class='btn-in-home-cards learn-drink-btn'>" + cocktails[drink]['name']+ "</button></a> <a  href=" + cocktails[drink]['quizUrl'] + "><button class='take-quiz-btn btn btn-in-home-cards'>Take the Quiz</button></a></div></div></div></div>") 
   $(".homeDynamicCardsRow").append(new_card)
 }
 
